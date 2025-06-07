2d369e991b19cd2985bdb0b9d59add335dd421df
DJ-x501 <bhargavdhananjaix501@gmail.com>
2025-06-07 11:10:20 +0530

    starting

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/home.css';
+
+const Home = () => {
+    return (
+        <div className='wrapper'>
+            <div className="textContainer">
+                <div className="leftText">
+                    <div className="head">DHANANJAY</div>
+                </div>
+                <div className="rightText">
+                    <div className="subHead">KUMAR</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
